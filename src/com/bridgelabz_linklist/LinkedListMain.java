af9896255236d6b0585af428eede96fc933add0e
champ09D <darshanmhatre709@gmail.com>
2022-07-09 21:52:37 +0530

    UC2_Ability_To_Create_Linked_List_By_Adding_ File Done

--- src/com/bridgelabz_linklist/LinkedListMain.java
+++ src/com/bridgelabz_linklist/LinkedListMain.java
@@ -2,24 +2,14 @@ package com.bridgelabz_linklist;
 import java.util.*;
 public class LinkedListMain
 {
-    public static void main(String[] argv) throws Exception
+
+    public static void main(String[] darsh)
     {
-        try {
-
-            LinkedList<Integer> list = new LinkedList<Integer>();
-
-            // add some elements to list
-            list.add(56);
-            list.add(30);
-            list.add(70);
-
-            System.out.println("LinkedList : " + list);
-            int value = list.element();
-            System.out.println("Head of list : " + value);
-        }
-        catch (NullPointerException e)
-        {
-            System.out.println("Exception thrown : " + e);
-        }
+        System.out.println("----------welcome to linked list program------------");
+        LinkedList list = new LinkedList();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        list.show();
     }
 }
