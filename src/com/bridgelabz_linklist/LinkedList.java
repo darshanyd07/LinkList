8613ac5a4fed30c104285c7776cf367d90ce53e5
champ09D <darshanmhatre709@gmail.com>
2022-07-09 22:04:06 +0530

    UC3_Ability_To_Create_Linke_List_By_Appending File Done

--- src/com/bridgelabz_linklist/LinkedList.java
+++ src/com/bridgelabz_linklist/LinkedList.java
@@ -1,5 +1,4 @@
 package com.bridgelabz_linklist;
-
 public class LinkedList
 {
 
@@ -7,6 +6,30 @@ public class LinkedList
     Node tail;
 
     public void add(int data)
+    {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void show()
+    {
+        Node  temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data +" ");
+            temp = temp.next;
+        }
+    }
+
+    public void append(int data)
     {
         Node newNode = new Node(data);
         if(head == null)
@@ -22,17 +45,7 @@ public class LinkedList
         }
     }
 
-    public void show()
-    {
-        Node  temp = head;
-        while(temp != null)
-        {
-            System.out.print(temp.data +" ");
-            temp = temp.next;
-        }
-    }
 }
-
 class Node
 {
 
@@ -45,3 +58,6 @@ class Node
         this.next = null;
     }
 }
+    Ability to create Linked
+    List by appending 30 and
+        70 to 56
--- src/com/bridgelabz_linklist/LinkedListMain.java
+++ src/com/bridgelabz_linklist/LinkedListMain.java
@@ -3,13 +3,14 @@ import java.util.*;
 public class LinkedListMain
 {
 
-    public static void main(String[] darsh)
-    {
-        System.out.println("----------welcome to linked list program------------");
+    public static void main(String[] args) {
+
+        System.out.println("welcome to linked list program");
         LinkedList list = new LinkedList();
-        list.add(56);
-        list.add(30);
-        list.add(70);
+        list.append(56);
+        list.append(30);
+        list.append(70);
         list.show();
     }
+
 }
