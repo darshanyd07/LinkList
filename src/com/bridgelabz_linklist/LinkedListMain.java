e45ab09ebc6892578ff95993267251853eace2a5
champ09D <darshanmhatre709@gmail.com>
2022-07-09 22:54:24 +0530

    UC8_Ability_to_Insert_40_After_30 File Done

--- src/com/bridgelabz_linklist/LinkedListMain.java
+++ src/com/bridgelabz_linklist/LinkedListMain.java
@@ -2,9 +2,7 @@ package com.bridgelabz_linklist;
 import java.util.*;
 public class LinkedListMain {
 
-
     public static void main(String[] args) {
-
         LinkedList list = new LinkedList();
         list.append(56);
         list.append(30);
@@ -14,11 +12,6 @@ public class LinkedListMain {
         list.insertAtMiddle(30);
         list.show();
         System.out.println();
-        list.popFirstelement();
-        list.show();
-
-        list.popLastelement();
-        list.show();
         System.out.println();
         if(list.searchNode(30)) {
             System.out.println("serach key found");
@@ -27,7 +20,10 @@ public class LinkedListMain {
             System.out.println("serach key not found");
         }
 
+        System.out.println();
+        list.insertAtPosition(40, 3);
+        list.show();
+
     }
 
 }
-
